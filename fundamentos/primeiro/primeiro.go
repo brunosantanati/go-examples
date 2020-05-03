91f4870d9bdc6ccd28fa12168b7052f4ffcb231c
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-05-03 14:20:28 -0300

    Rearrange folders structure.

--- fundamentos/primeiro/primeiro.go
+++ fundamentos/primeiro/primeiro.go
@@ -8,3 +8,8 @@ func main() {
 
 //para executar arquivo Go usando o plugin Code Runner do VS Code
 //ctrl + alt + n
+//ou via linha de comando:
+//go run "/home/bruno/Documentos/go/fundamentos/primeiro/primeiro.go"
+
+//para parar a execucao de algum programa
+//ctrl + alt + m
