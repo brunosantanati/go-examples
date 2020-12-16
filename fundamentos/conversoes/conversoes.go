b948e20c62511a52054d76158241b4b411e0f718
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-16 15:15:51 -0300

    Usando um apelido para o package importado.

--- fundamentos/conversoes/conversoes.go
+++ fundamentos/conversoes/conversoes.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	c "strconv"
 )
 
 func main() {
@@ -21,16 +21,16 @@ func main() {
 	fmt.Println("Teste " + string(123)) //mostra o caracter correspondente ao código 123 na tabela ASCII
 
 	//int para string
-	fmt.Println("Teste " + strconv.Itoa(123))
+	fmt.Println("Teste " + c.Itoa(123))
 	fmt.Println("Teste", 123)
 
 	//string para int
-	num, _ := strconv.Atoi("123") //converte a string "123" para int e atribui a variável num. O _ seria para receber o erro, mas estamos ignorando por enquanto
+	num, _ := c.Atoi("123") //converte a string "123" para int e atribui a variável num. O _ seria para receber o erro, mas estamos ignorando por enquanto
 	fmt.Println(num - 122)
 
 	//string to bool
-	b, _ := strconv.ParseBool("true")
-	//b, _ := strconv.ParseBool("1") //1 mesmo que true, qualquer outro número é false
+	b, _ := c.ParseBool("true")
+	//b, _ := c.ParseBool("1") //1 mesmo que true, qualquer outro número é false
 	if b {
 		fmt.Println("Verdadeiro")
 	}
