cbf7bebe5e1148a9a6438b28e2cc400e04a818f5
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-29 14:57:30 -0300

    Exemplo if com inicializacao.

---
+++ controles/ifinit/ifinit.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func numeroAleatorio() int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+	return r.Intn(10)
+}
+
+func main() {
+	if i := numeroAleatorio(); i > 5 { // tb suportado no switch
+		fmt.Println("Ganhou!!!")
+		fmt.Println(i)
+	} else {
+		fmt.Println("Perdeu!")
+		fmt.Println(i)
+	}
+}
