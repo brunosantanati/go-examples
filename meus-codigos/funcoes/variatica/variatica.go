d749f2f18282f641f162ea47ac18184afecb309d
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-31 11:51:04 -0300

    Exemplo de funcao variatica(como varargs do Java).

---
+++ meus-codigos/funcoes/variatica/variatica.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func media(numeros ...float64) float64 {
+	total := 0.0
+
+	for _, num := range numeros {
+		fmt.Println(num)
+		total += num
+	}
+
+	fmt.Printf("Total: %.2f\n", total)
+
+	return total / float64(len(numeros))
+}
+
+func main() {
+	fmt.Printf("Média: %.2f", media(7.7, 8.1, 5.9, 9.9))
+}
