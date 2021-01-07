c94a87c8ed3fb8fda87af3073624428292e4da80
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-01-07 14:46:00 -0300

    Utilizando pacote do Github.

---
+++ meus-codigos/pacote/usandolib/usandolib.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/brunosantanati/goarea"
+	"github.com/brunosantanati/goarea/abc"
+)
+
+// antes rodar comando abaixo dentro de C:\Users\55119\go:
+// go get -u github.com/brunosantanati/goarea
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+	abc.Epa()
+}
