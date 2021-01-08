3b350c96791840092a6227e781ba209be8ffb7eb
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-01-08 17:42:04 -0300

    Add instrucoes como rodar e acessar o server.

--- meus-codigos/http/serverdb/server.go
+++ meus-codigos/http/serverdb/server.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+// Rodar no terminal: go run *.go
+// URL todos usuários: http://localhost:3000/usuarios/
+// URL usuário específico(passar id): http://localhost:3000/usuarios/1
+
 func main() {
 	http.HandleFunc("/usuarios/", UsuarioHandler)
 	log.Println("Executando...")
